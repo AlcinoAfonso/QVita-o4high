16b0431b4f601cbdcf303b036fcc2792d2965e52
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-05-30 15:17:50 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 // pages/index.js
 
-import FeatureSplit from '../components/FeatureSplit'
+import FeatureSection from '../components/FeatureSection'
 
 export default function Home() {
   return (
     <>
-      <FeatureSplit
+      <FeatureSection
         title="Terapia Quântica: Como Funciona"
-        items={[
+        features={[
           '🧠 A terapia busca identificar padrões sutis — emocionais, mentais, físicos ou energéticos — que influenciam a vida da pessoa, mesmo sem que ela perceba de forma consciente.',
           '⚖️ Esses desequilíbrios podem estar por trás de conflitos, bloqueios ou desconfortos. Lidar com essas causas ajuda a promover mudanças reais no cotidiano.',
           '🔄 Cada pessoa reage de forma única, por isso os resultados podem variar de acordo com a experiência e o momento de cada um.',
           '🌱 A terapia não é uma promessa, mas tende a funcionar melhor quando haja abertura para se observar, escutar a si mesmo e aplicar pequenas mudanças conscientes no seu dia a dia.',
         ]}
-        imgSrc="https://quantecportal.com/wp-content/uploads/2025/03/image-18-300x300.webp"
+        imageUrl="https://quantecportal.com/wp-content/uploads/2025/03/image-18-300x300.webp"
+        inverse={false}
       />
     </>
   )
